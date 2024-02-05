0dca6db3fe6fb04a9cb2fe2d1ac584f89a4fb045
Zeshan Ahmed <ze.ahmed.x@protonmail.com>
2024-02-05 12:13:09 +0500

    Header Modified

---
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Header } from '../../../../payload/payload-types'
+
+const HeaderMobileNav = ({ header }: { header: Header }) => {
+  return (
+    <div>HeaderMobileNav</div>
+  )
+}
+
+export default HeaderMobileNav
